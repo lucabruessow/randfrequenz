48088331773a779d6531d23d9dc2a7e0ddc27b9a
Luca Brüssow <210828907+lucabruessow@users.noreply.github.com>
2026-05-17 18:44:07 +0200

    feat: add interactive greeting message using preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://randfrequenz.netlify.app"
+  site: "https://randfrequenz.netlify.app",
+  integrations: [preact()]
 });
